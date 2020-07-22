fc73de8562e112425e8ea56a8890c232b6cd5b7b
rubenperegrina <peregrinaruben@gmail.com>
2020-07-22 13:16:52 +0200

    first commit

---
+++ src/app/_models/team.ts
@@ -0,0 +1,8 @@
+import { TeamsModule } from "@app/teams/teams.module"
+
+export class Team {
+    name: String;
+    location: String;
+    type: String;
+    namePhases: String;
+}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { AuthGuard } from './_helpers';
+
+const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
+const teamsModule = () => import('./teams/teams.module').then(x => x.TeamsModule);
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'users', loadChildren: usersModule, canActivate: [AuthGuard] },
+    { path: 'teams', loadChildren: teamsModule, canActivate: [AuthGuard] },
+    { path: 'account', loadChildren: accountModule },
+
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
